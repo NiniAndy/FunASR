67a958add87b94cd8dbad6c1a0d91083698fb831
NiniAndy <ice.four2@gmail.com>
2024-07-25 20:13:48 +0800

    更新llm—asr训练参数

--- examples/industrial_data_pretraining/llm_asr/demo_train_or_finetune.sh
+++ examples/industrial_data_pretraining/llm_asr/demo_train_or_finetune.sh
@@ -3,7 +3,7 @@
 
 
 # which gpu to train or finetune
-export CUDA_VISIBLE_DEVICES="0"
+export CUDA_VISIBLE_DEVICES="0,1,2,3"
 gpu_num=$(echo $CUDA_VISIBLE_DEVICES | awk -F "," '{print NF}')
 
 # data dir, which contains: train.json, val.json, tokens.jsonl/tokens.txt, am.mvn
@@ -15,45 +15,31 @@ gpu_num=$(echo $CUDA_VISIBLE_DEVICES | awk -F "," '{print NF}')
 #++data_type_list='["source", "target"]' \
 #++jsonl_file_out=/Users/zhifu/funasr1.0/test_local/audio_datasets.jsonl
 
-train_data="/nfs/maziyang.mzy/data/librispeech/librispeech_train_960h.jsonl"
-val_data="/nfs/maziyang.mzy/data/librispeech/librispeech_dev_other_filtered.jsonl"
+train_data="/ssd/zhuang/code/FunASR2024/examples/librispeech/DATA/data/train-960/audio_datasets.jsonl"
+val_data="/ssd/zhuang/code/FunASR2024/examples/librispeech/DATA/data/dev-other/audio_datasets.jsonl"
 
 # exp output dir
-output_dir="/nfs/zhifu.gzf/ckpt/exp/llm_asr_whisper_vicuna_exp1"
+output_dir="/ssd/zhuang/code/FunASR2024/examples/industrial_data_pretraining/llm_asr/exp/llm_asr_whisper_qwen_exp1"
 log_file="${output_dir}/log.txt"
 
 workspace=`pwd`
-config="whisper_vicuna_linear.yaml"
+config="whisper_qwen_linear.yaml"
 
 init_param="${output_dir}/model.pt"
 
 mkdir -p ${output_dir}
 echo "log_file: ${log_file}"
 
-deepspeed_config=${workspace}/../../ds_stage1.json
-
-DISTRIBUTED_ARGS="
-    --nnodes ${WORLD_SIZE:-1} \
-    --nproc_per_node $gpu_num \
-    --node_rank ${RANK:-0} \
-    --master_addr ${MASTER_ADDR:-127.0.0.1} \
-    --master_port ${MASTER_PORT:-26669}
-"
-
-echo $DISTRIBUTED_ARGS
-
-torchrun $DISTRIBUTED_ARGS \
-../../../funasr/bin/train_ds.py \
+torchrun \
+--nnodes 1 \
+--nproc_per_node ${gpu_num} \
+../../../funasr/bin/train.py \
 --config-path "${workspace}/conf" \
 --config-name "${config}" \
 ++train_data_set_list="${train_data}" \
 ++valid_data_set_list="${val_data}" \
-++dataset_conf.batch_size=4 \
+++dataset_conf.batch_size=2 \
 ++dataset_conf.num_workers=4 \
-++train_conf.max_epoch=15 \
-++train_conf.use_deepspeed=false \
-++train_conf.deepspeed_config=${deepspeed_config} \
+++train_conf.max_epoch=20 \
 ++optim_conf.lr=0.0001 \
-++init_param="${init_param}" \
-
 ++output_dir="${output_dir}" &> ${log_file} &
